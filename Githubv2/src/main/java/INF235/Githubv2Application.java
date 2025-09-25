3e571011051851563063041f8f62fd82ee60b1a8
Raph <johndoe@example.com>
2025-09-26 01:17:54 +0800

    idk

---
+++ Githubv2/src/main/java/INF235/Githubv2Application.java
@@ -0,0 +1,13 @@
+package INF235;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Githubv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Githubv2Application.class, args);
+	}
+
+}
